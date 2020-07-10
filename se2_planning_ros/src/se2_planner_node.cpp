8c3384a936ad24edcef41e605faedf8e9e054f54
meychr <meyerc@student.ethz.ch>
2020-07-10 15:50:05 +0200

    Publish test gridmap

--- se2_planning_ros/src/se2_planner_node.cpp
+++ se2_planning_ros/src/se2_planner_node.cpp
@@ -7,6 +7,12 @@
 
 #include <ros/ros.h>
 
+//#include <grid_map_core/iterators/GridMapIterator.hpp>
+//#include "grid_map_core/GridMap.hpp"
+//#include "grid_map_core/iterators/PolygonIterator.hpp"
+//#include "grid_map_msgs/GridMap.h"
+#include <grid_map_ros/grid_map_ros.hpp>
+#include "se2_planning/GridMapLazyStateValidator.hpp"
 #include "se2_planning_ros/loaders.hpp"
 
 int main(int argc, char** argv) {
@@ -15,11 +21,36 @@ int main(int argc, char** argv) {
   ros::init(argc, argv, "se2_planner_node");
   ros::NodeHandlePtr nh(new ros::NodeHandle("~"));
 
+  ros::Publisher publisher = nh->advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
+
   std::string filename = nh->param<std::string>("/ompl_planner_ros/parameter_path", "ompl_rs_planner_ros/nav_msgs_path");
   const auto plannerParameters = loadOmplReedsSheppPlannerParameters(filename);
   const auto plannerRosParameters = loadOmplReedsSheppPlannerRosParameters(filename);
   auto planner = std::make_shared<OmplReedsSheppPlanner>();
   planner->setParameters(plannerParameters);
+
+  // Grid map update has to be thread safe?
+  std::string layerName = "traversability";
+  grid_map::GridMap gridMap;
+  gridMap.setFrameId("world");
+  gridMap.setGeometry(grid_map::Length(20.0, 20.0), 0.1, grid_map::Position(0, 0));
+
+  gridMap.add(layerName, 0.0);
+  for (grid_map::GridMapIterator iterator(gridMap); !iterator.isPastEnd(); ++iterator) {
+    grid_map::Position position;
+    gridMap.getPosition(*iterator, position);
+    if (position.x() < 7 && position.x() > 5 && fabs(position.y()) < 2) {
+      gridMap.at(layerName, *iterator) = 1.0;  // obstacles
+    }
+  }
+  grid_map_msgs::GridMap message;
+  grid_map::GridMapRosConverter::toMessage(gridMap, message);
+  publisher.publish(message);
+  // std::unique_ptr<StateValidator> validator = se2_planning::createGridMapLazyStateValidator(gridMap,
+  // se2_planning::computeFootprint(1.0, 0.0, 0.5, 0.5), layerName);
+  planner->setStateValidator(
+      se2_planning::createGridMapLazyStateValidator(gridMap, se2_planning::computeFootprint(1.0, 0.0, 0.5, 0.5), layerName));
+
   se2_planning::OmplReedsSheppPlannerRos plannerRos(nh);
   plannerRos.setPlanningStrategy(planner);
   plannerRos.setParameters(plannerRosParameters);
